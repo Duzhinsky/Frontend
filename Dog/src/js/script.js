96224494fbc79b2166772da05e8d3b4d4a4fd70d
Duzhinsky <w1vvv1w@ya.ru>
2021-07-28 04:34:16 +0700

    add products section, pixel-perfect

--- Dog/src/js/script.js
+++ Dog/src/js/script.js
@@ -4,3 +4,9 @@ $('.header__extra__burger').on('click', function(e) {
 	  $('.main-page__menu-content').toggleClass("menu-active");
       e.preventDefault();
 });
+
+$(".product__img").get().forEach(function(entry, index, array) {
+    var cw = $(entry).width();
+	$(entry).css({'height':cw*0.75675675675+'px'});
+	console.log(cw);
+});
