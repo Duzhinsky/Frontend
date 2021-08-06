52da55548f1130b796d4aff4800cd01aaa5ada97
Duzhinsky <w1vvv1w@ya.ru>
2021-08-07 06:49:44 +0700

    add phone-input styles

--- plitka/gulpfile.js
+++ plitka/gulpfile.js
@@ -78,7 +78,7 @@ function js(params) {
 		.pipe(dest(path.build.js))
 		.pipe(uglify())
 		.pipe(rename({
-			extname: ".min.css"
+			extname: ".min.js"
 		}))
 		.pipe(dest(path.build.js))
 		.pipe(browsersync.stream());
