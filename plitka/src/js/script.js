1e704fdfc70f9bac62211ecbbe11aa8e6ca3cb5a
Duzhinsky <w1vvv1w@ya.ru>
2021-08-07 07:00:11 +0700

    fix mask

--- plitka/src/js/script.js
+++ plitka/src/js/script.js
@@ -3,6 +3,8 @@
 let phone_inputs = document.querySelectorAll('.phone-input');
 phone_inputs.forEach(input => {
 	Inputmask({
-		"mask": "+7 (999) 999-99-99"
+		mask: "+7 (999) 999-99-99",
+		placeholder: "0",
+		clearMaskOnLostFocus: false,
 	}).mask(input);
 });
