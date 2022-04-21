2a1479399cd00d5990f46fb3d1dff975bfc32a18
Duzhinsky <w1vvv1w@ya.ru>
2022-04-21 12:18:28 +0300

    rm  draft pages

--- academic/PiperNet_hw/js/burger.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.header__burger').click(function() {
-        $('.header__burger, .header__menu').toggleClass('active')
-    })
-    $('.header__menu__link').click(function() {
-        $('.header__burger, .header__menu').removeClass('active')
-    })
-});
